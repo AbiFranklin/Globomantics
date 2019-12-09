d6de03df3462c4f8f914d49684cee9cc0c5930a9
AbiFranklin <Abifranklin@gmail.com>
2019-12-09 15:09:56 -0600

    Finished through mid Mod 2

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './main-page.css';
+import Header from './header'
+import { thisTypeAnnotation } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    this.fetchHouses();
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(res => res.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+      this.setState({ loading: false });
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    }
+  }
+  
+  render() { 
+    return ( 
+      this.state.loading ?
+      <h1>Loading ...</h1> :
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
